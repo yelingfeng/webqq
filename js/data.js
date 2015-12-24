b016f1beb9cc45fc04302bd66852211a06ea5947
renxiaofan <yelingfeng521@gmail.com>
2015-12-24 10:28:49 +0800

    add

---
+++ js/data.js
@@ -0,0 +1,211 @@
+﻿DATA={
+	app:{//桌面1
+		'readGod':{
+			appid:'2534',
+			icon:'readGod.png',
+			name:'读览天下',
+			asc :1
+		},
+		'sosomap':{
+			appid:'42',
+			icon:'sosomap.png',
+			name:'搜搜地图',
+			asc :2
+		},
+		'jinshan':{
+			appid:'8992',
+			icon:'jinshan.png',
+			name:'金山快盘',
+			asc :3
+		},
+		'douban':{
+			appid:'3402',
+			icon:'douban.png',
+			name:'豆瓣电台',
+			asc :4
+		},
+		'Pixlr':{
+			appid:'64',
+			icon:'pixlr.png',
+			name:'Pixlr',
+			asc:5
+		},
+		'weather':{
+			appid:'18',
+			icon:'5.png',
+			name:'天气',
+			asc:6
+		},
+		'Clock':{
+			appid:'20',
+			icon:'8.png',
+			name:'时钟',
+			asc:7,
+		},
+		'yule':{
+			appid:'514',
+			icon:'folder_o.png',
+			name:'娱乐',
+			asc:8
+		},
+		'mangguo':{
+			appid:'3401',
+			icon:'mangguo.png',
+			name:'芒果旅游',
+			asc :1
+		},
+		'tuanmap':{
+			appid:'2527',
+			icon:'tuanmap.png',
+			name:'团购地图',
+			asc :2
+		},
+		'fastsearch':{
+			appid:'3693',
+			icon:'fastsearch.png',
+			name:'快递查询',
+			asc :3
+		},
+		'bianqian':{
+			appid:'10',
+			icon:'bianqian.png',
+			name:'便签',
+			asc :4
+		},
+		'wangdesk':{
+			appid:'13',
+			icon:'wangdesk.png',
+			name:'网络硬盘',
+			asc :5
+		},
+		'friend':{
+			appid:'21',
+			icon:'friend.png',
+			name:'奥尔网',
+			asc :1
+		},
+		'friendnear':{
+			appid:'31',
+			icon:'friendnear.png',
+			name:'好友近况',
+			asc :2
+		},
+		'friendgroup':{
+			appid:'32',
+			icon:'friendgroup.png',
+			name:'好友管理',
+			asc :3
+		},
+		'kaikai':{
+			appid:'2250',
+			icon:'kaikai.png',
+			name:'开开',
+			asc :4
+		},
+		'kxjy':{
+			appid:'2535',
+			icon:'kxjy.png',
+			name:'奥尔交友',
+			asc :5
+		},
+		'qidianzhongwen':{
+			appid:'2528',
+			icon:'qidianzhongwen.png',
+			name:'起点中文',
+			asc :1
+		},
+		'qqread':{
+			appid:'45',
+			icon:'qqread.png',
+			name:'奥尔阅读',
+			asc :2
+		},
+		'xiami':{
+			appid:'2526',
+			icon:'xiami.png',
+			name:'虾米电台',
+			asc :3
+		},
+		'musicbox':{
+			appid:'56',
+			icon:'musicbox.png',
+			name:'音乐盒子',
+			asc :4
+		},
+		'vadio':{
+			appid:'15',
+			icon:'vadio.png',
+			name:'奥尔视频',
+			asc :5
+		},
+		'leshi':{
+			appid:'3148',
+			icon:'leshi.png',
+			name:'乐视网',
+			asc :6
+		},
+		'doudizhi':{
+			appid:'48',
+			icon:'doudizhu.png',
+			name:'欢乐斗地主',
+			asc :1
+		},
+		'3366':{
+			appid:'49',
+			icon:'3366.png',
+			name:'3366',
+			asc :2
+		},
+		'qqbaby':{
+			appid:'26',
+			icon:'qqbaby.png',
+			name:'QQ宝贝',
+			asc :3
+		},
+		'game':{
+			appid:'1000',
+			icon:'game.png',
+			name:'便签',
+			asc :4
+		}
+	},
+	sApp:{//侧边栏应用
+		'appmarket':{
+			appid:'1',
+			icon:'appmarket.png',
+			name:'应用市场',
+			asc :1
+		},
+		'qq':{
+			appid:'2',
+			icon:'big.png',
+			name:'QQ',
+			asc :2
+		},
+		'weibo':{
+			appid:'3',
+			icon:'weibo.png',
+			name:'微博',
+			asc :3
+		},
+		'mail':{
+			appid:'4',
+			icon:'mail.png',			
+			name:'邮箱',
+			asc :4
+		},
+		'zone':{
+			appid:'5',
+			icon:'zone.png',
+			name:'空间',
+			asc :5
+		},
+		'internet':{
+			appid:'6',
+			icon:'internet.png',
+			name:'浏览网页',
+			asc :6
+		}
+	
+	}
+};
